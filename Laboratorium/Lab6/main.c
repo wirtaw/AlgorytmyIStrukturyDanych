cc682719e8f3651c444f436e70a87b5e0b477f8f
SebasPav <115025994+SebasPav@users.noreply.github.com>
2022-12-20 18:50:31 +0200

    Update main.c

--- Laboratorium/Lab6/main.c
+++ Laboratorium/Lab6/main.c
@@ -11,6 +11,7 @@ Sebastian Pavlovič
 #include "selection.c" // selectionSort(array, size)
 // selection sort nie dziala calkowicie poprawnie nie wiem z jakiej przyczyny
 #include "merge.c" // mergeSort(arr, 0, size - 1)
+#include "quick.c" // quickSort(arr, 0, size - 1)
 
 int main(void) {
   int tab[] = {5, 2, 5, 4, 8, 7, 5, 6, 9, 5, 2, 1, 4, 6, 5, 4, 8, 5, 3, 6, 9, 8, 7, 1, 8, 4, 0, 9, 8, 7, 4, 9, 8, 7, 4, 2, 3, 6, 4, 8, 7, 5, 6, 7, 2, 1, 5, 8, 1};
